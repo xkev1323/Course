152976f5ea99662b0df66c8c4e22533fa4109171
Kevin Hernandez <kevin.alfonso.hernandez.amaya@gmail.com>
2024-10-28 18:38:36 -0700

    Nuevos Scripts
    Scripts de prueba para el nuevo flujo de trabajo (GitHub)

---
+++ Assets/Course/11_Control de Versiones/Example.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Course.ControlDeVersiones
+{
+    public class Example : MonoBehaviour
+    {
+        int id;
+
+        private void Start()
+        {
+            id = 1;   
+        }
+    }
+}
